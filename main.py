8e4cfa1a35c5fb442edab76ca4bdb33fec40fe91
Shreejith Kumar Das S <135417968+ArtsyDEV-C@users.noreply.github.com>
2023-10-10 15:11:51 +0530

    Update challenge 2pointone.py

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
- # Python program to check if year is a leap year or not
-
-year = 2000
-
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
-
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
-
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
-
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+#program for create objects batsman and bowler classes and call the play() method for each objects
+#create base class player
+class player:
+  def play(self):
+    print("The player is playing cricket.")
+#create derived class batsman
+class Batsman(player):
+  def play(self):
+    print("The batsman is batting.")
+#create derived class bowler
+class Bowler(player):
+  def play(self):
+    print("The bowler is bowling.")
+#creating objects
+batsman = Batsman()
+bowler = Bowler()
+#call the method play()
+batsman.play()
+bowler.play()
